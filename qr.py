c234a38d084e5c0cb94008673b1db2410e5ba239
Raul Escamilla <raul.escamilla@concentrix.com>
2022-11-12 15:28:35 -0600

    vsv-logo

---
+++ qr.py
@@ -0,0 +1,47 @@
+# import modules
+import qrcode
+from PIL import Image
+ 
+# taking image which user wants
+# in the QR code center
+Logo_link = 'ALTA.jpg'
+ 
+logo = Image.open(Logo_link)
+ 
+# taking base width
+basewidth = 180
+ 
+# adjust image size
+wpercent = (basewidth/float(logo.size[0]))
+hsize = int((float(logo.size[1])*float(wpercent)))
+
+logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
+QRcode = qrcode.QRCode(
+    error_correction=qrcode.constants.ERROR_CORRECT_H
+)
+ 
+# taking url or text
+url = 'https://www.vsvlegal.com/en/eo-el-salvador-noviembre-2022/'
+ 
+# adding URL or text to QRcode
+QRcode.add_data(url)
+ 
+# generating QR code
+QRcode.make()
+ 
+# taking color name from user
+QRcolor = (15, 36, 63)
+ 
+# adding color to QR code
+QRimg = QRcode.make_image(
+    fill_color=QRcolor, back_color="white").convert('RGB')
+ 
+# set size of QR code
+pos = ((QRimg.size[0] - logo.size[0]) // 2,
+       (QRimg.size[1] - logo.size[1]) // 2)
+QRimg.paste(logo, pos)
+ 
+# save the QR code generated
+QRimg.save('data/qr-code.png')
+ 
+print('QR code generated!')
